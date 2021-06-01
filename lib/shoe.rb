4fb584e862e8e25507d156fc9ad0e1c50dd74b73
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-06-01 23:02:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class shoe
+attr_accessor :brand, :color, :size, :material, :condition
+def initialize(brand)
+  @brand = brand
+end
+def cobble
+  puts "The shoe has been repaired."
+  @condition = "new"
+end
+
+end
